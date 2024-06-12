4b2188a2afbf41ea48fb884acf03e948fe484971
jrms <160367299+ByteRaider@users.noreply.github.com>
2024-06-12 08:31:41 -0700

    Use Template https://github.com/ByteRaider/formValidation_productsApp_FlutterExample as a starting point

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,22 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         title: "Products App",
-        home: const LoginScreen(),
+        //home: const LoginScreen(),
+        initialRoute: 'home',
         debugShowCheckedModeBanner: false,
         routes: {
           'login': (context) => const LoginScreen(),
           'home': (context) => const HomeScreen(),
         },
         theme: ThemeData.dark().copyWith(
-          scaffoldBackgroundColor: const Color.fromARGB(255, 48, 55, 98),
-          iconTheme: IconThemeData(color: Colors.green.shade200),
-        ));
+            scaffoldBackgroundColor: const Color.fromARGB(255, 48, 55, 98),
+            iconTheme: IconThemeData(color: Colors.green.shade200),
+            appBarTheme: const AppBarTheme(
+              color: Color.fromARGB(255, 48, 55, 98),
+              elevation: 0,
+            ),
+            floatingActionButtonTheme: const FloatingActionButtonThemeData(
+              backgroundColor: Color.fromARGB(255, 69, 71, 86),
+            )));
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/widgets.dart';
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({superkey});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Welcome to HomeScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Products'),
+      ),
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (BuildContext context, int index) => const ProductCard(),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {},
       ),
     );
   }
