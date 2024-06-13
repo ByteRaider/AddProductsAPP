79f16cd0592a5aff7ed37eb89ae93c35a27944a7
jrms <160367299+ByteRaider@users.noreply.github.com>
2024-06-13 07:59:29 -0700

    Add positioned back and camera button - no logic

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -6,12 +6,45 @@ class ProductScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
+        // Scrollable widget
         child: Column(
+          // Column widget
           children: [
+            // List of widgets
             Stack(
-              children: [ProductImage()],
+              // Stack allows you to place multiple widgets on top of each other
+              children: [
+                const ProductImage(), // product image
+                Positioned(
+                  left: 20,
+                  top: 60,
+                  child: IconButton(
+                    // GoBack button
+                    onPressed: () => Navigator.of(context).pop(), // go back
+                    //onPressed: () =>  Navigator.pop(context),
+                    icon: const Icon(
+                      Icons.arrow_back_ios_new,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  right: 20,
+                  top: 60,
+                  child: IconButton(
+                    // Camera or gallery button
+                    onPressed: () {
+                      //TODO: CAMERA OR GALLERY FUNCTION
+                    },
+                    icon: const Icon(
+                      Icons.camera_alt,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ],
             )
           ],
         ),
