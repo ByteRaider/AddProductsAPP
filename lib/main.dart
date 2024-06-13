e3bb581313d08af2d1101997cebd46558874125b
jrms <160367299+ByteRaider@users.noreply.github.com>
2024-06-13 13:11:46 -0700

    Add AppState to main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:form_validation_productsapp_example/services/services.dart';
+import 'package:provider/provider.dart';
 import 'screens/screens.dart';
 
-void main() => runApp(const MyApp());
+void main() => runApp(const AppState());
+
+class AppState extends StatelessWidget {
+  const AppState({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => ProductService(),
+        ),
+      ],
+      child: const MyApp(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
