d989e913bb9f3f021094b08c7958ef48b88432d6
jrms <160367299+ByteRaider@users.noreply.github.com>
2024-06-14 20:16:39 -0700

    Create loadProducts

--- lib/services/product_service.dart
+++ lib/services/product_service.dart
@@ -1,8 +1,23 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:form_validation_productsapp_example/models/models.dart';
+import 'package:http/http.dart' as http;
 
 class ProductService extends ChangeNotifier {
   final String _baseUrl = 'https://varios-975e0-default-rtdb.firebaseio.com';
   final List<Product> products = [];
-  //TODO: fetch products
+  bool isLoading = true;
+
+  ProductService() {
+    loadProducts();
+  }
+
+  // <List<Product>>
+  Future loadProducts() async {
+    final url = Uri.https(_baseUrl, 'products.json');
+    final resp = await http.get(url);
+    final Map<String, dynamic> productsMap = json.decode(resp.body);
+    print(productsMap);
+  }
 }
