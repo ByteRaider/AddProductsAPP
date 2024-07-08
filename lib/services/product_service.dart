c59b791938e646a47520cb478a1dcf7a1e9b12fc
ByteRaider <160367299+ByteRaider@users.noreply.github.com>
2024-07-07 22:07:32 -0700

    Edit loadProducts

--- lib/services/product_service.dart
+++ lib/services/product_service.dart
@@ -5,7 +5,7 @@ import 'package:form_validation_productsapp_example/models/models.dart';
 import 'package:http/http.dart' as http;
 
 class ProductService extends ChangeNotifier {
-  final String _baseUrl = 'https://varios-975e0-default-rtdb.firebaseio.com';
+  final String _baseUrl = 'varios-975e0-default-rtdb.firebaseio.com';
   final List<Product> products = [];
   bool isLoading = true;
 
@@ -18,6 +18,13 @@ class ProductService extends ChangeNotifier {
     final url = Uri.https(_baseUrl, 'products.json');
     final resp = await http.get(url);
     final Map<String, dynamic> productsMap = json.decode(resp.body);
-    print(productsMap);
+    productsMap.forEach(
+      (key, value) {
+        final tempProduct = Product.fromJson(value);
+        tempProduct.id = key;
+        this.products.add(tempProduct);
+      },
+    );
+    print(this.products[0]);
   }
 }
