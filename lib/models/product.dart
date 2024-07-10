0e4e20349a2731bfbc8887427f51d07618ebeafa
ByteRaider <160367299+ByteRaider@users.noreply.github.com>
2024-07-10 08:04:40 -0700

    Edit Product model

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,56 +1,32 @@
 import 'dart:convert';
 
 class Product {
-  String? id;
-  Cheeseburger cheeseburger;
-  Cheeseburger torta;
-
-  Product({
-    required this.cheeseburger,
-    required this.torta,
-  });
-
-  factory Product.fromRawJson(String str) => Product.fromJson(json.decode(str));
-
-  String toRawJson() => json.encode(toJson());
-
-  factory Product.fromJson(Map<String, dynamic> json) => Product(
-        cheeseburger: Cheeseburger.fromJson(json["cheeseburger"]),
-        torta: Cheeseburger.fromJson(json["torta"]),
-      );
-
-  Map<String, dynamic> toJson() => {
-        "cheeseburger": cheeseburger.toJson(),
-        "torta": torta.toJson(),
-      };
-}
-
-class Cheeseburger {
   bool available;
   String name;
-  String? picture;
+  String picture;
   double price;
+  String? id;
 
-  Cheeseburger({
+  Product({
     required this.available,
     required this.name,
-    this.picture,
+    required this.picture,
     required this.price,
+    this.id,
   });
 
-  factory Cheeseburger.fromRawJson(String str) =>
-      Cheeseburger.fromJson(json.decode(str));
+  factory Product.fromJson(String str) => Product.fromMap(json.decode(str));
 
-  String toRawJson() => json.encode(toJson());
+  String toJson() => json.encode(toMap());
 
-  factory Cheeseburger.fromJson(Map<String, dynamic> json) => Cheeseburger(
+  factory Product.fromMap(Map<String, dynamic> json) => Product(
         available: json["available"],
         name: json["name"],
         picture: json["picture"],
         price: json["price"]?.toDouble(),
       );
 
-  Map<String, dynamic> toJson() => {
+  Map<String, dynamic> toMap() => {
         "available": available,
         "name": name,
         "picture": picture,
